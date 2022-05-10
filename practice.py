f07c1395f08525cc3bcb0e34af1e01b55c493fdd
GGODungiejjo <92909292+GGODungiejjo@users.noreply.github.com>
2022-05-10 11:24:09 +0900

    ball center

--- practice.py
+++ practice.py
@@ -1,6 +1,5 @@
-# Import a library of functions called 'pygame'
-
 import pygame
+
 # 1. 게임 초기화
 pygame.init()
 
@@ -14,28 +13,12 @@ pygame.display.set_caption(title)
 # 3. 게임 내 필요한 설정
 clock = pygame.time.Clock()
 
-class obj:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-    def put_img(self, address):
-        if address[-3:] == "png":
-            self.img = pygame.image.load(address).convert_alpha()
-        else :
-            self.img = pygame.image.load(address)
-        self.sx, self.sy = self.img.get_size()
-    def change_size(self, sx, sy):
-        self.img = pygame.transform.scale(self.img, (sx, sy))
-        self.sx, self.sy = self.img.get_size()
-    def show(self):
-        screen.blit(self.img, (self.x,self.y))
-
-ss = obj()
-ss.put_img("D:/temp/ss.png")
-ss.change_size(50,80)
-ss.x = round(size[0]/2- ss.sx/2)
-ss.y = size[1] -ss.sy - 15
-
+ss = pygame.image.load("C:/Users/jjojj/Desktop/Ai project/ball.png").convert_alpha()
+#ss는 기본 공임
+ss = pygame.transform.scale(ss,(15,15))
+ss_sx,ss_sy = ss.get_size()
+ss_x = round(size[0]/2)-ss_sx
+ss_y = round(size[1])-200-ss_sy
 
 black = (0,0,0)
 white = (255,255,255)
@@ -56,10 +39,9 @@ while SB == 0:
     # 4-3. 입력, 시간에 따른 변화
     k += 1
 
-
     # 4-4. 그리기
     screen.fill(black)
-    ss.show()
+    screen.blit(ss,(ss_x,ss_y))
 
     # 4-5. 업데이트
     pygame.display.flip()
